72ba824bdd0f9b4931695153c522105987aa4c0e
carlosvilam <carlos.vilam@alumnos.upm.es>
2019-05-18 15:46:22 +0200

    koopa

---
+++ SuperMarioBros2D/Assets/Scripts/TurtleCollision.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TurtleCollision : MonoBehaviour
+{
+    public Turtle turtle;
+    public Mario mario;
+    private float nextJump = 0f;
+    private float jumpRate = 0.2f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Mario")
+        {
+            if (turtle.dead)
+            {
+                if (Time.time > nextJump)
+                {
+                    turtle.deadcollision = true;
+
+                    if (turtle.gameObject.transform.position.x < mario.gameObject.transform.position.x)
+                    {
+                        turtle.direction = 1;
+                    }
+                    else
+                    {
+                        turtle.direction = -1;
+                    }
+                }
+            }
+
+            else
+            {
+                mario.hit();
+            }
+        }
+
+        else
+        {
+            if (collision.gameObject.tag != "Turtle")
+            {
+                if (!turtle.dead)
+                {
+                    turtle.direction = -turtle.direction;
+                }
+
+                else if (turtle.dead && turtle.deadcollision)
+                {
+
+                    if (collision.gameObject.tag == "Goomba")
+                    {
+                        Destroy(collision.gameObject);
+                    }
+                    else if (collision.gameObject.tag == "Mario")
+                    {
+                        mario.hit();
+                    }
+                    else
+                    {
+
+                        turtle.direction = -turtle.direction;
+                    }
+                }
+            }
+        }
+    }
+
+}
