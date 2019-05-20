e30c241e2f49883c645b1b5c378847df4e4a36c4
carlosvilam <carlos.vilam@alumnos.upm.es>
2019-05-20 18:44:20 +0200

    adding koopa sounds

--- SuperMarioBros2D/Assets/Scripts/TurtleCollision.cs
+++ SuperMarioBros2D/Assets/Scripts/TurtleCollision.cs
@@ -6,34 +6,42 @@ using System;
 public class TurtleCollision : MonoBehaviour
 {
     public Turtle turtle;
-    public Mario mario;
     private float nextJump = 0f;
     private float jumpRate = 0.2f;
+    private AudioSource sound;
+    public AudioClip rebote;
+
+    void Start() {
+        sound = GetComponent<AudioSource>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Mario")
         {
-            if (turtle.dead)
+            if (turtle.dead && !turtle.deadcollision)
             {
                 if (Time.time > nextJump)
                 {
                     turtle.deadcollision = true;
 
-                    if (turtle.gameObject.transform.position.x < mario.gameObject.transform.position.x)
-                    {
-                        turtle.direction = 1;
-                    }
-                    else
+                    if (turtle.gameObject.transform.position.x < collision.gameObject.transform.position.x)
                     {
                         turtle.direction = -1;
                     }
+                    else
+                    {
+                        turtle.direction = 1;
+                    }
                 }
+            }
+            else if (turtle.deadcollision) {
+                collision.gameObject.GetComponent<Mario>().hit();
             }
 
             else
             {
-                mario.hit();
+                collision.gameObject.GetComponent<Mario>().hit();
             }
         }
 
@@ -48,18 +56,14 @@ public class TurtleCollision : MonoBehaviour
 
                 else if (turtle.dead && turtle.deadcollision)
                 {
-
-                    if (collision.gameObject.tag == "Goomba")
+                    
+                    if (collision.gameObject.tag == "Wumpa")
                     {
-                        Destroy(collision.gameObject);
-                    }
-                    else if (collision.gameObject.tag == "Mario")
-                    {
-                        mario.hit();
+                        collision.gameObject.GetComponent<Goomba>().dead();
                     }
                     else
                     {
-
+                        sound.PlayOneShot(rebote, 1f);
                         turtle.direction = -turtle.direction;
                     }
                 }
